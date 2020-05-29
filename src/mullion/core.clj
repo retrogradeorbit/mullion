4887174c2c519d255d77a82376f8438ac0bf4ee5
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-29 23:23:02 +0800

    launch a gui

--- src/mullion/core.clj
+++ src/mullion/core.clj
@@ -1,6 +1,8 @@
 (ns mullion.core
-  (:require [mullion.libs]
+  (:require [mullion.libs :as libs]
             [mullion.reflection])
+  (:import [org.bytedeco.qt.Qt5Widgets QApplication QTextEdit]
+           [org.bytedeco.javacpp PointerPointer IntPointer])
   (:gen-class))
 
 (set! *warn-on-reflection* true)
@@ -8,4 +10,21 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (libs/init!)
+  (libs/load-libs)
+  (let [lib-dir (libs/get-lib-dir)
+        app (QApplication.
+             (IntPointer. (int-array [3]))
+             (PointerPointer.
+              (into-array
+               String
+               ["gettingstarted"
+                "-platformpluginpath"
+                lib-dir])
+              ))
+        text-edit (QTextEdit.)]
+    (.show text-edit)
+    (System/exit (QApplication/exec))
+
+    )
+  )
