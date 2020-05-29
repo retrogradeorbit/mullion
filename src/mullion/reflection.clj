c0834916f1049aad68875421f0227918c8848ffa
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-29 23:27:29 +0800

    all declared methods and classes, not just public

--- src/mullion/reflection.clj
+++ src/mullion/reflection.clj
@@ -4,129 +4,129 @@
 ;; Write out graal reflection configs
 
 (def jni-reflection
-  {'org.bytedeco.javacpp.Loader {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.Pointer {:conf #{:all-public-*}
+  {'org.bytedeco.javacpp.Loader {:conf #{:all}}
+   'org.bytedeco.javacpp.Pointer {:conf #{:all}
                                   :fields #{'address 'position 'limit
                                             'capacity 'deallocator}}
-   'org.bytedeco.javacpp.BytePointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.ShortPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.IntPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.LongPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.FloatPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.DoublePointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.CharPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.BooleanPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.PointerPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.BoolPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.CLongPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.SizeTPointer {:conf #{:all-public-*}}
-   'java.lang.UnsatisfiedLinkError {:conf #{:all-public-*}}
-   'java.lang.String {:conf #{:all-public-*}}
-   'java.lang.NullPointerException {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.Pointer$NativeDeallocator {:conf #{:all-public-*}
+   'org.bytedeco.javacpp.BytePointer {:conf #{:all}}
+   'org.bytedeco.javacpp.ShortPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.IntPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.LongPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.FloatPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.DoublePointer {:conf #{:all}}
+   'org.bytedeco.javacpp.CharPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.BooleanPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.PointerPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.BoolPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.CLongPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.SizeTPointer {:conf #{:all}}
+   'java.lang.UnsatisfiedLinkError {:conf #{:all}}
+   'java.lang.String {:conf #{:all}}
+   'java.lang.NullPointerException {:conf #{:all}}
+   'org.bytedeco.javacpp.Pointer$NativeDeallocator {:conf #{:all}
                                                     :fields #{'ownerAddress}}
-   'java.nio.Buffer {:conf #{:all-public-*}
+   'java.nio.Buffer {:conf #{:all}
                      :fields #{'ownerAddress 'position 'limit 'capacity}}
-   'org.bytedeco.javacpp.tools.Generator$BooleanEnum {:conf #{:all-public-*}
+   'org.bytedeco.javacpp.tools.Generator$BooleanEnum {:conf #{:all}
                                                       :fields #{'value}}
-   'org.bytedeco.javacpp.tools.Generator$ByteEnum {:conf #{:all-public-*}
+   'org.bytedeco.javacpp.tools.Generator$ByteEnum {:conf #{:all}
                                                    :fields #{'value}}
-   'org.bytedeco.javacpp.tools.Generator$ShortEnum {:conf #{:all-public-*}
+   'org.bytedeco.javacpp.tools.Generator$ShortEnum {:conf #{:all}
                                                     :fields #{'value}}
-   'org.bytedeco.javacpp.tools.Generator$IntEnum {:conf #{:all-public-*}
+   'org.bytedeco.javacpp.tools.Generator$IntEnum {:conf #{:all}
                                                   :fields #{'value}}
-   'org.bytedeco.javacpp.tools.Generator$LongEnum {:conf #{:all-public-*}
+   'org.bytedeco.javacpp.tools.Generator$LongEnum {:conf #{:all}
                                                    :fields #{'value}}})
 
 (def svm-reflection
-  {'org.bytedeco.javacpp.presets.javacpp {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.Loader {:conf #{:all-public-*}}
-   'org.bytedeco.qt.global.Qt5Core {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.Pointer {:conf #{:all-public-*}
+  {'org.bytedeco.javacpp.presets.javacpp {:conf #{:all}}
+   'org.bytedeco.javacpp.Loader {:conf #{:all}}
+   'org.bytedeco.qt.global.Qt5Core {:conf #{:all}}
+   'org.bytedeco.javacpp.Pointer {:conf #{:all}
                                   :fields #{'address 'position 'limit
                                             'capacity 'deallocator}}
-   'org.bytedeco.javacpp.BytePointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.ShortPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.IntPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.LongPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.FloatPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.DoublePointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.CharPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.BooleanPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.PointerPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.BoolPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.CLongPointer {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.SizeTPointer {:conf #{:all-public-*}}
-   'java.lang.UnsatisfiedLinkError {:conf #{:all-public-*}}
-   'java.lang.String {:conf #{:all-public-*}}
-   'org.bytedeco.javacpp.Pointer$NativeDeallocator {:conf #{:all-public-*}
+   'org.bytedeco.javacpp.BytePointer {:conf #{:all}}
+   'org.bytedeco.javacpp.ShortPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.IntPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.LongPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.FloatPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.DoublePointer {:conf #{:all}}
+   'org.bytedeco.javacpp.CharPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.BooleanPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.PointerPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.BoolPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.CLongPointer {:conf #{:all}}
+   'org.bytedeco.javacpp.SizeTPointer {:conf #{:all}}
+   'java.lang.UnsatisfiedLinkError {:conf #{:all}}
+   'java.lang.String {:conf #{:all}}
+   'org.bytedeco.javacpp.Pointer$NativeDeallocator {:conf #{:all}
                                                     :fields #{'ownerAddress}}
-   'java.nio.Buffer {:conf #{:all-public-*}
+   'java.nio.Buffer {:conf #{:all}
                      :fields #{'ownerAddress 'position 'limit 'capacity}}
-   'java.lang.NullPointerException {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QAbstractEventDispatcher$TimerInfo {:conf #{:all-public-*}}
-   'java.lang.RuntimeException {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QAbstractEventDispatcher {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QByteArray {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QObject {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QChildEvent {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QString {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QCoreApplication {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QDeferredDeleteEvent {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QDynamicPropertyChangeEvent {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QEvent {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QEvent$Type {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QEventLoop {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QMessageLogContext {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QMessageLogger {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QSize {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QTimerEvent {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QVariant {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Core.QtMessageHandler {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Gui.QIcon {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Gui.QCloseEvent {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QAbstractButton {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QWidget {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QAbstractScrollArea {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QAbstractSpinBox {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Gui.QFont {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QMenu {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QAction {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QStyle {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QApplication {:conf #{:all-public-*}}
-   'org.bytedeco.qt.helper.Qt5Widgets$QLayoutItem {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QBoxLayout {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QCheckBox {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QLineEdit {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QComboBox {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QDialog {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QFrame {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QGridLayout {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QGroupBox {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QHBoxLayout {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QLabel {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QLayout {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QPushButton {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QMessageBox {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QSizePolicy$ControlType {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QSizePolicy {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QAbstractSpinBox$StepType {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QSpinBox {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QSystemTrayIcon {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QTextEdit$ExtraSelection {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QTextEdit {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QToolButton {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QVBoxLayout {:conf #{:all-public-*}}
-   'org.bytedeco.qt.helper.Qt5Gui$QPaintDevice {:conf #{:all-public-*}}
-   'org.bytedeco.qt.helper.Qt5Widgets$MessageClickedCallback {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Widgets.QSystemTrayIcon$ActivationReason {:conf #{:all-public-*}}
-   'org.bytedeco.qt.helper.Qt5Widgets$ActivatedCallback {:conf #{:all-public-*}}
-   'org.bytedeco.qt.helper.Qt5Widgets$CurrentIndexChangedCallback1 {:conf #{:all-public-*}}
-   'org.bytedeco.qt.helper.Qt5Widgets$TriggeredCallback {:conf #{:all-public-*}}
-   'org.bytedeco.qt.helper.Qt5Widgets$ToggledCallback {:conf #{:all-public-*}}
-   'org.bytedeco.qt.helper.Qt5Widgets$ClickedCallback {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Gui.QGuiApplication {:conf #{:all-public-*}}
-   'org.bytedeco.qt.Qt5Gui.QTextCodec {:conf #{:all-public-*}}
+   'java.lang.NullPointerException {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QAbstractEventDispatcher$TimerInfo {:conf #{:all}}
+   'java.lang.RuntimeException {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QAbstractEventDispatcher {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QByteArray {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QObject {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QChildEvent {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QString {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QCoreApplication {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QDeferredDeleteEvent {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QDynamicPropertyChangeEvent {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QEvent {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QEvent$Type {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QEventLoop {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QMessageLogContext {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QMessageLogger {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QSize {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QTimerEvent {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QVariant {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Core.QtMessageHandler {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Gui.QIcon {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Gui.QCloseEvent {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QAbstractButton {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QWidget {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QAbstractScrollArea {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QAbstractSpinBox {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Gui.QFont {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QMenu {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QAction {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QStyle {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QApplication {:conf #{:all}}
+   'org.bytedeco.qt.helper.Qt5Widgets$QLayoutItem {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QBoxLayout {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QCheckBox {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QLineEdit {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QComboBox {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QDialog {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QFrame {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QGridLayout {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QGroupBox {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QHBoxLayout {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QLabel {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QLayout {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QPushButton {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QMessageBox {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QSizePolicy$ControlType {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QSizePolicy {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QAbstractSpinBox$StepType {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QSpinBox {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QSystemTrayIcon {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QTextEdit$ExtraSelection {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QTextEdit {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QToolButton {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QVBoxLayout {:conf #{:all}}
+   'org.bytedeco.qt.helper.Qt5Gui$QPaintDevice {:conf #{:all}}
+   'org.bytedeco.qt.helper.Qt5Widgets$MessageClickedCallback {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Widgets.QSystemTrayIcon$ActivationReason {:conf #{:all}}
+   'org.bytedeco.qt.helper.Qt5Widgets$ActivatedCallback {:conf #{:all}}
+   'org.bytedeco.qt.helper.Qt5Widgets$CurrentIndexChangedCallback1 {:conf #{:all}}
+   'org.bytedeco.qt.helper.Qt5Widgets$TriggeredCallback {:conf #{:all}}
+   'org.bytedeco.qt.helper.Qt5Widgets$ToggledCallback {:conf #{:all}}
+   'org.bytedeco.qt.helper.Qt5Widgets$ClickedCallback {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Gui.QGuiApplication {:conf #{:all}}
+   'org.bytedeco.qt.Qt5Gui.QTextCodec {:conf #{:all}}
    })
 
 (defn make-reflection-json [reflection-config]
@@ -134,11 +134,15 @@
         (->> reflection-config
              (map (fn [[sym {:keys [conf fields]}]]
                     (let [struct {"name" (name sym)}
-                          struct (if (conf :all-public-*)
+                          struct (if (conf :all)
                                    (assoc struct
                                           "allPublicMethods" true
                                           "allPublicConstructors" true
-                                          "allPublicClasses" true)
+                                          "allPublicClasses" true
+                                          "allDeclaredMethods" true
+                                          "allDeclaredConstructors" true
+                                          "allDeclaredClasses" true
+                                          )
                                    struct)
                           struct (if fields
                                    (assoc struct
