693a3e938e7635abb830953342a29fe8b63ad164
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-29 21:53:37 +0800

    port a bunch of decloj over

--- src/mullion/core.clj
+++ src/mullion/core.clj
@@ -1,5 +1,8 @@
 (ns mullion.core
+  (:require [mullion.libs])
   (:gen-class))
+
+(set! *warn-on-reflection* true)
 
 (defn -main
   "I don't do a whole lot ... yet."
---
+++ src/mullion/libs.clj
@@ -0,0 +1,220 @@
+(ns mullion.libs
+  (:require [clojure.string :as string]
+            [cheshire.core :as cheshire]
+            [clojure.java.io :as io])
+  (:import [org.bytedeco.javacpp Loader]
+           [org.bytedeco.qt.global Qt5Core]
+           [org.bytedeco.qt.Qt5Widgets QApplication QTextEdit]
+           [java.nio.file Paths Files LinkOption]
+           [java.nio.file.attribute FileAttribute]))
+
+(set! *warn-on-reflection* true)
+
+(def config-dir ".mullion")
+
+(def loader-properties (Loader/loadProperties))
+
+(defn get-property [property]
+  (.getProperty ^java.util.Properties loader-properties property))
+
+(def property-platform (get-property "platform"))
+(def property-library-prefix (get-property "platform.library.prefix"))
+(def property-library-suffix
+  (-> (get-property "platform.library.suffix")
+      (string/split #":")
+      first))
+
+(def linux? (string/starts-with? property-platform "linux"))
+(def macos? (string/starts-with? property-platform "macos"))
+(def windows? (string/starts-with? property-platform "windows"))
+
+(assert (not windows?) "Windows platform not supported.")
+
+(def resource-libs
+  [
+   {:path "org/bytedeco/javacpp/"
+    :names ["jnijavacpp"]}
+
+   {:path "org/bytedeco/qt/"
+    :names
+    (cond
+      linux?
+      [
+       "Qt5DBus@.5" "Qt5Gui@.5" "Qt5XcbQpa@.5" "Qt5Widgets@.5" "Qt5PrintSupport@.5"
+       "Qt5Core@.5" "jniQt5Core" "jniQt5Widgets" "qxdgdesktopportal" "qxcb"
+       "qlinuxfb" "qminimalegl" "qminimal" "qoffscreen" "composeplatforminputcontextplugin"
+       "ibusplatforminputcontextplugin" "qxcb-egl-integration" "qxcb-glx-integration"
+       "qgif" "qico" "qjpeg" "qevdevkeyboardplugin" "qevdevmouseplugin" "qevdevtabletplugin"
+       "qevdevtouchplugin" "jniQt5Gui"]
+
+      macos?
+      [
+       "Qt5DBus@.5" "Qt5Gui@.5" "Qt5Widgets@.5" "Qt5PrintSupport@.5" "Qt5Core@.5"
+       "jniQt5Core" "jniQt5Widgets" "qcocoa" "cocoaprintersupport" "qxdgdesktopportal"
+       "qminimal" "qoffscreen" "qgif" "qico" "qjpeg" "jniQt5Gui"])}])
+
+(defn make-lib-resource-path [path name]
+  (let [[lib suffix] (string/split name #"@")]
+    (case property-platform
+      "macosx-x86_64"
+      (str path property-platform "/"
+           property-library-prefix
+           lib
+           suffix
+           property-library-suffix)
+
+      "linux-x86_64"
+      (str path property-platform "/"
+           property-library-prefix
+           lib
+           property-library-suffix
+           suffix))))
+
+(defn make-lib-link-name [name]
+  (let [[lib _] (string/split name #"@")]
+    (str property-library-prefix
+         lib
+         property-library-suffix)))
+
+(defn make-lib-file-name [name]
+  (let [[lib suffix] (string/split name #"@")]
+    (case property-platform
+      "macosx-x86_64"
+      (str property-library-prefix
+           lib
+           suffix
+           property-library-suffix)
+
+      "linux-x86_64"
+      (str property-library-prefix
+           lib
+           property-library-suffix
+           suffix))))
+
+(defn make-resources-config-hashmap []
+  {"resources"
+   (->> resource-libs
+        (map (fn [{:keys [path names]}]
+               (for [n names]
+                 (make-lib-resource-path path n))))
+        (flatten)
+        (map (fn [fname]
+               {"pattern" (str (string/replace fname #"\." "\\\\.") "$")}))
+        (into []))
+   "bundles" []})
+
+(defn write-resources-config-hashmap [filename]
+  (spit filename
+        (cheshire/generate-string
+         (make-resources-config-hashmap)
+         {:pretty true})))
+
+(defonce _write-resources
+  (when-not (System/getProperty "org.graalvm.nativeimage.imagecode")
+    (write-resources-config-hashmap "graal-configs/resource-config.json")))
+
+(def library-load-list
+  (->> resource-libs
+       (map :names)
+       flatten
+       (map #(first (string/split % #"@")))))
+
+
+(defn path-split
+  "give a full path filename, return a tuple of
+  [path basename]
+
+  eg \"blog/posts/1/main.yml\" -> [\"blog/posts/1\" \"main.yml\"]
+  "
+  [filename]
+  (let [file (io/file filename)]
+    [(.getParent file) (.getName file)]))
+
+(defn path-join
+  "given multiple file path parts, join them all together with the
+  file separator"
+  [& parts]
+  (.getPath ^java.io.File (apply io/file parts)))
+
+(def empty-string-array
+  (make-array String 0))
+
+(def empty-file-attribute-array
+  (make-array FileAttribute 0))
+
+(def empty-link-options
+  (make-array LinkOption 0))
+
+(def no-follow-links
+  (into-array LinkOption [LinkOption/NOFOLLOW_LINKS]))
+
+(defn symlink [link target]
+  (let [link-path (Paths/get link empty-string-array)]
+    (when (Files/exists link-path no-follow-links)
+      ;; link path exists
+      (if (Files/isSymbolicLink link-path)
+        ;; and its a symlink
+        (Files/delete link-path)
+
+        ;; its something else
+        (throw (ex-info (str link " already exists and is not a symlink")
+                        {:path link}))))
+
+    (.toString
+     (Files/createSymbolicLink
+      link-path
+      (Paths/get target empty-string-array)
+      empty-file-attribute-array))))
+
+(defn setup
+  "Copy any of the bundled dynamic libs from resources to the
+  run time lib directory"
+  [libs-dir]
+  (doseq [{:keys [filename
+                  linkname
+                  resource-file]} (->> resource-libs
+                                  (map second)
+                                  (map (fn [{:keys [path names]}]
+                                         (for [n names]
+                                           (let [resource-file (make-lib-resource-path path n)
+                                                 link-name (make-lib-link-name n)
+                                                 filename (make-lib-file-name n)]
+                                             (if (not= link-name filename)
+                                               {:resource-file resource-file
+                                                :filename filename
+                                                :linkname link-name}
+                                               {:resource-file resource-file
+                                                :filename filename})))))
+                                  (flatten))]
+    (when-let [file (io/resource resource-file)]
+      ;; write out the filename if needed
+      (let [[_ name] (path-split (.getFile file))
+            dest-path (path-join libs-dir name)
+            resource-size (with-open [out (java.io.ByteArrayOutputStream.)]
+                            (io/copy (io/input-stream file) out)
+                            (count (.toByteArray out)))]
+        ;; writing to a library while running its code can result in segfault
+        ;; only write if filesize is different or it doesnt exist
+        (when (or (not (.exists (io/file dest-path)))
+                  (not= (.length (io/file dest-path)) resource-size))
+          (println "writing" resource-size "bytes to" dest-path)
+          (io/copy (io/input-stream file) (io/file dest-path))))
+
+      ;; if a symlink is needed, make it
+      (when linkname
+        (println "symlinking" (path-join libs-dir linkname) "to" filename)
+        (symlink (path-join libs-dir linkname) filename)))))
+
+(defn init! []
+  (let [native-image?
+        (and (= "Substrate VM" (System/getProperty "java.vm.name"))
+             (= "runtime" (System/getProperty "org.graalvm.nativeimage.imagecode")))
+        home-dir (System/getenv "HOME")
+        config-dir (path-join home-dir config-dir)
+        libs-dir (path-join config-dir "libs")]
+    (.mkdirs (io/as-file libs-dir))
+
+    (when native-image?
+      (setup libs-dir)
+      (println "setting java.library.path to:" libs-dir)
+      (System/setProperty "java.library.path" libs-dir))))
