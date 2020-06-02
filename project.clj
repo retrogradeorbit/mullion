c3e78ffed178633ebff0bd8cfc4d5242493c13e3
Crispin Wellington <retrogradeorbit@gmail.com>
2020-06-02 16:33:03 +0800

    use latest snapshots for windows build

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
-                 [org.bytedeco/qt-platform "5.14.2-1.5.3"]
+                 #_ [org.bytedeco/qt-platform "5.14.2-1.5.3"]
+                 [org.bytedeco/qt-platform "5.15.0-1.5.4-SNAPSHOT"]
                  [cheshire "5.10.0"]]
+  :repositories [["sonatype" {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
   :main ^:skip-aot mullion.core
   :java-source-paths ["src/java"]
   :jvm-opts [#=(str "-Djava.library.path=" #= (java.lang.System/getenv "HOME") "/.mullion/libs")
