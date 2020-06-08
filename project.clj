d575ff9e64477fca779a62f33bb1c61918b7a603
Crispin Wellington <retrogradeorbit@gmail.com>
2020-06-08 23:07:47 +0800

    use local built version

--- project.clj
+++ project.clj
@@ -5,12 +5,21 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
                  #_ [org.bytedeco/qt-platform "5.14.2-1.5.3"]
+
+                 ;; from sonatype
+                 ;;[org.bytedeco/qt-platform "5.15.0-1.5.4-SNAPSHOT"]
+
+                 ;; local install
+                 [org.bytedeco/javacpp "1.5.4-SNAPSHOT"]
+                 [org.bytedeco/javacpp-platform "1.5.4-SNAPSHOT"]
+                 [org.bytedeco/qt "5.15.0-1.5.4-SNAPSHOT"]
                  [org.bytedeco/qt-platform "5.15.0-1.5.4-SNAPSHOT"]
+
                  [cheshire "5.10.0"]
                  [mvxcvi/puget "1.2.0"]
 
                  ]
-  :repositories [["sonatype" {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
+  ;;:repositories [["sonatype" {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
   :main ^:skip-aot mullion.core
   :java-source-paths ["src/java"]
   :jvm-opts [#=(str "-Djava.library.path=" #= (java.lang.System/getenv "HOME") "/.mullion/libs")
